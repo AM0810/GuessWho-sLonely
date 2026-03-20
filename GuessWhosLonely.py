14daafef26ac89d8337bf4a736f39351c47093cb
AM0810 <ws417904@weston.ac.uk>
2026-03-20 14:30:30 +0000

    Worked on hair length question and developed a process of elimination system to get a definite answer

--- GuessWhosLonely.py
+++ GuessWhosLonely.py
@@ -2,21 +2,38 @@ import pandas as pd
 from random import randint
 charactersdf = pd.read_csv(open('Names.csv', "r"))
 
+# Main menu if the computer is the one guessing the player's character
 def ComputerGuesserMenu():
-    HairLengths = ["Long", "Medium", "Short"]
-    HairColours = ["Blonde", "Brown", "Black", "Ginger", "Silver", "Pink", "Purple"]
+    # storing lists of possible values for each attribute
+    HairLengths = ["long", "medium", "short"]
+    HairColours = ["blonde", "brown", "black", "ginger", "silver", "pink", "purple"]
 
+    # handles the "Does your character have (x) length hair?" question
     def HairLengthQuestionFunction():
-        RandomHairLength = randint(0, len(HairLengths))
-        HairLengthQuestion = input(f"Does your character have {HairLengths[RandomHairLength]} hair? (yes/no): ").lower()
-        HairLengths.remove(HairLengths[RandomHairLength])
-        return HairLengthQuestion
+        
+        def HairLengthRandomiser():
+            # If the length of the list is bigger than 1
+            if len(HairLengths) > 1:
+                # Generate a random number between 0 and the length of the list minus one (to prevent an index error as indexes start at 0)
+                RandomHairLength = randint(0, len(HairLengths)-1)
+                # HairLength stores the randomised hair length ...
+                HairLength = HairLengths[RandomHairLength]
+                # ... then removes that value from the list, so the question can't be repeated.
+                HairLengths.remove(HairLength)
+            # If the length of the list is smaller or equal to 1
+            else:
+                # The hair length *must* be the only one remaining in the list, as the others have been ruled out.
+                HairLength = HairLengths[0]
+            return HairLength
+        HairLength = HairLengthRandomiser()
+        Question = input(f"Does your character have {HairLength} length hair? (yes/no): ").lower()
+        return Question
 
     def HairColourQuestionFunction():
-        RandomHairLength = randint(0, len(HairLengths))
-        HairLengthQuestion = input(f"Does your character have {HairLengths[RandomHairLength]} hair? (yes/no): ").lower()
-        HairLengths.remove(HairLengths[RandomHairLength])
-        return HairLengthQuestion
+        RandomHairColour = randint(0, len(HairColours))
+        Question = input(f"Does your character have {HairColours[RandomHairColour]} coloured hair? (yes/no): ").lower()
+        HairColours.remove(HairColours[RandomHairColour])
+        return Question
 
 
     print("The computer is now the guesser.")
@@ -31,6 +48,13 @@ def ComputerGuesserMenu():
             break
         if response == "n" or response == "no":
             print ("Well, you better think of somebody!")
+        # handles unexpected inputs
         else:
             print("Invalid input! Try again.")
 
+    while True:
+        HairLengthQuestion = HairLengthQuestionFunction()
+
+    # HairColourQuestion = HairColourQuestionFunction()
+
+ComputerGuesserMenu()
