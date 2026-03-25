76d9d57c1f71f4bb8446f2864e2636c10ac0dc51
W1kozak <ws416458@weston.ac.uk>
2026-03-25 10:31:48 +0000

    Continued on menu and fixed csv

--- menu.py
+++ menu.py
@@ -1,17 +1,31 @@
 Facial_Features = ["Hair colour", "Skin Colour", "Facial Hair", "Hair Length", "Eye Colour"]
+Hair_Colour = ["Blonde", "Brown", "Black", "Ginger", "Grey", "Purple", "Pink"]
+Skin_Colour = ["White", "Black"]
+Facial_Hair = ["Yes", "No"]
+Hair_Length = ["Short", "Medium", "Long"]
+Eye_Colour = ["Blue", "Green", "Brown", "Black", "Purple"]
 Accessories = []
 
 selection = False
 while selection == False:
-    Choice = input("What features would you like to ask about (1) facial features or (2) accessories")
+    Choice = input("What features would you like to ask about (1) facial features or (2) accessories\n")
     if Choice == "1":
         selection = True
         Choice = int(input("what features about the face would you like to ask about?\n(1) Hair colour\n(2) Skin Colour\n(3) Facial Hair\n(4) Hair Length\n(5) Eye Colour\n"))
         if Facial_Features[Choice-1] in Facial_Features:
             print(f"You have selected {Facial_Features[Choice-1]}")
-            if Facial_Features[Choice-1] == Facial_Features[0]:
-                Choice = int(input("What Hair colour do you think they have?\n(1) Blonde\n(2) Brown\n(3) Black\n(4) Ginger\n(5) Grey\n(6) Purple\n(7) Pink\n"))
-        else: 
+            
+        elif Facial_Features[Choice-1] == Facial_Features[0]:
+                print(f"You have selected {Facial_Features[Choice-1]}")
+        elif Facial_Features[Choice-1] == Facial_Features[1]:
+                print(f"You have selected {Facial_Features[Choice-1]}")
+        elif Facial_Features[Choice-1] == Facial_Features[2]:
+                print(f"You have selected {Facial_Features[Choice-1]}")
+        elif Facial_Features[Choice-1] == Facial_Features[3]:
+                print(f"You have selected {Facial_Features[Choice-1]}")
+        elif Facial_Features[Choice-1] == Facial_Features[4]:
+                print(f"You have selected {Facial_Features[Choice-1]}")
+        else:
             print("invalid")
     elif Choice == "2":
         selection = True
